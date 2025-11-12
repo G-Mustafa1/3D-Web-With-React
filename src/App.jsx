0caf27596d4c1db96529049fe6a28e0d6ebad657
Ghulam Mustafa <gmustufa1255@gmail.com>
2025-11-12 19:30:26 +0500

    My3DStudio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { Suspense, useState } from 'react'
+import { Suspense, lazy } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './layout/Layout'
-import Home from './pages/Home'
+// import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Services from './pages/Services'
 import Loader from './components/Loader'
+const Home = lazy(() => import("./pages/Home"))
 
 function App() {
   return (
